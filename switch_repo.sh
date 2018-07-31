c5a7e05c58a7591db4581b296f88eedbcac344a5
iGor Polyakov <igor_polyakov@phystech.edu>
2018-07-31 11:21:50 +0300

    script works correctlt

--- switch_repo.sh
+++ switch_repo.sh
@@ -7,18 +7,28 @@ mkdir 1/src
 mkdir 1/src/app
 mkdir 1/src/app/main
 
+mkdir 2
+mkdir 2/src
+mkdir 2/src/app
+mkdir 2/src/app/main
+
 
 for file_name in angular.json package.json src/app/main/main.component.html src/index.html; do
   sed "s/abracadabra/$new_project_name/g" < $file_name > "1/$file_name"
 done
 
-main_component="1/src/app/main/main.component.html"
-sed "s/abracadabora_repo/$new_repo/g" < $main_component > $main_component
+main_component=
+sed "s/abracadabora_repo/$new_repo/g" < "1/src/app/main/main.component.html" > "2/src/app/main/main.component.html"
 
-# for file_name in 1/angular.json 1/package.json 1/src/app/main/main.component.html 1/src/index.html; do
-#   old_file_name=
-#   sed "s/abracadabra/$new_project_name/g" < $file_name > $(echo $file_name | cut -c 3-)
-# done
+mv "2/src/app/main/main.component.html" "1/src/app/main/main.component.html"
+
+
+for file_name in 1/angular.json 1/package.json 1/src/app/main/main.component.html 1/src/index.html; do
+  old_file_name=$(echo $file_name | cut -c 3-)
+  sed "s/abracadabra/$new_project_name/g" < $file_name > $old_file_name
+done
+
+rm -rf 1 2
 
 # changing project folder name
 # mv ../ngTemplate "../$new_project_name"
