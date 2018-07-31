8c21387ebf0605ad3fe030a92e432ffbc6938cb3
iGor Polyakov <igor_polyakov@phystech.edu>
2018-07-31 11:14:43 +0300

    script updated

--- switch_repo.sh
+++ switch_repo.sh
@@ -1,9 +1,24 @@
 # changing all project-name recordings in all files
 new_repo=new_repo
 new_project_name=new_project_name
+
+mkdir 1
+mkdir 1/src
+mkdir 1/src/app
+mkdir 1/src/app/main
+
+
 for file_name in angular.json package.json src/app/main/main.component.html src/index.html; do
-  echo $(sed "s/abracadabra/$new_project_name/g" < $file_name) > $file_name
+  sed "s/abracadabra/$new_project_name/g" < $file_name > "1/$file_name"
 done
+
+main_component="1/src/app/main/main.component.html"
+sed "s/abracadabora_repo/$new_repo/g" < $main_component > $main_component
+
+# for file_name in 1/angular.json 1/package.json 1/src/app/main/main.component.html 1/src/index.html; do
+#   old_file_name=
+#   sed "s/abracadabra/$new_project_name/g" < $file_name > $(echo $file_name | cut -c 3-)
+# done
 
 # changing project folder name
 # mv ../ngTemplate "../$new_project_name"
