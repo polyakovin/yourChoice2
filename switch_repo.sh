bee92a242ac7e3c7b7ea04591e31f7ce924e1624
iGor Polyakov <igor_polyakov@phystech.edu>
2018-07-31 10:47:32 +0300

    renaming script updated

--- switch_repo.sh
+++ switch_repo.sh
@@ -1,7 +1,19 @@
-# rename project name
-  # all project-name recordings in all files
-  # git repositiory
-  # git commit
-  # git push
-  # project folder name
-  # npm i
+# changing all project-name recordings in all files
+new_repo=new_repo
+new_project_name=new_project_name
+for file_name in angular.json package.json src/app/main/main.component.html src/index.html; do
+  echo $(sed "s/abracadabra/$new_project_name/g" < $file_name) > $file_name
+done
+
+# changing project folder name
+# mv ../ngTemplate "../$new_project_name"
+
+# changing git repositiory
+# git remote remove origin
+# git remote add origin $new_repo
+# git add .
+# git commit -m "project renamed to $new_project_name"
+# git push --set-upstream origin master
+
+# install all the dependencies
+# npm i
